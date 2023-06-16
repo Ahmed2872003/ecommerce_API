008db140552b71a9001581892fde23b36716c25c
Your Name <you@example.com>
2023-06-16 04:00:52 +0300

    improved error handler
    made it able to handle validation errors in sequelize

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,9 +1,27 @@
+const customAPIError = require("../errors/customError.js");
+const { StatusCodes } = require("http-status-codes");
 const errorHandler = (err, req, res, next) => {
   const customError = {
     statusCode: err.statusCode || 500,
     msg: err.message,
   };
-  res.status(customError.statusCode).json({ msg: err.message });
+  // handle sequelize validations errors
+  const { errors } = err;
+
+  if (errors) {
+    // len validation error
+    const error = errors[0];
+    if (error["validatorKey"] === "len") {
+      customError.msg = `Validation error: ${error["path"]} must be from ${error["validatorArgs"][0]} to ${error["validatorArgs"][1]} characters`;
+    }
+    // notNull constraint error
+    else if (error["validatorKey"] === "is_null") {
+      customError.msg = `Validation error: ${error["path"]} field can't be empty`;
+    }
+    customError.statusCode = StatusCodes.BAD_REQUEST;
+  }
+
+  res.status(customError.statusCode).json(customError.msg);
 };
 
 module.exports = errorHandler;
