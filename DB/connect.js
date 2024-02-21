ecbaf628e09dfcfa1c8770c72f76d247cf9ed908
= <=>
2024-02-21 17:06:44 +0200

    .

--- DB/connect.js
+++ DB/connect.js
@@ -1,22 +1,20 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(
-  "Ecommerce_machinery",
-  "Ecommerce_machinery",
-  process.env.DB_PASS,
-  {
-    host: "wec.h.filess.io",
-    dialect: "mysql",
-    logging: false,
-    port: 3307,
-    pool: {
-      max: 3,
-      idle: 160000,
-      evict: 180000,
-      acquire: 160000,
-    },
-  }
-);
+const sequelize = new Sequelize({
+  host: "wec.h.filess.io",
+  database: "Ecommerce_machinery",
+  username: "Ecommerce_machinery",
+  password: process.env.DB_PASS,
+  dialect: "mysql",
+  logging: false,
+  port: 3307,
+  pool: {
+    max: 3,
+    idle: 160000,
+    evict: 180000,
+    acquire: 160000,
+  },
+});
 
 // const sequelize = new Sequelize("ecommerce", "root", "Falcon2025adam1", {
 //   host: "localhost",
