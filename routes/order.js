5055d57f73b02cb76e388e94c2b09c87351224bf
= <=>
2024-08-12 12:51:27 +0200

    .

--- routes/order.js
+++ routes/order.js
@@ -1,12 +1,14 @@
 const router = require("express").Router();
 
+const iSeller = require("../middleware/isSeller.js");
+
 const {
   getCustomerOrderProducts,
-  getSellerOrderProoducts,
+  getIncomingOrderProducts,
 } = require("../controller/orderProducts.js");
 
 router.route("/").get(getCustomerOrderProducts);
 
-router.get("/seller", getSellerOrderProoducts);
+router.get("/incoming", iSeller, getIncomingOrderProducts);
 
 module.exports = router;
